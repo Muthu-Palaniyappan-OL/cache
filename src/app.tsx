1b6e91a088384e55a34e5a7d07c0bf7925ede270
Muthu Palaniyappan OL <muthu892542@gmail.com>
2024-02-14 16:38:22 +0530

    All 5 Content Uploaded

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,29 @@
-import { useState } from "preact/hooks";
 import publications from "./publications";
 
 // prettier-ignore
 export function App() {
-  const [index, _] = useState(0);
+  const years = [...new Set(publications.map(publication => (new Date(publication.date)).getFullYear()))].sort().reverse()
 
   return (
     <div class="font-Azonix">
       <Nav />
-      {/* @ts-ignore */}
-      <div class="_df_book w-full" id="flipbook_example" source={publications[index].file}></div>
-      {/* <div class="_df_book w-full" id="flipbook_example" source={"/cache" + publications[index].file}></div> */}
+      {years.map(year => {
+        return <div class="p-2">
+          <p class="m-2">{year}</p>
+            <div class="flex flex-row flex-wrap">
+            {publications.filter(publication => (new Date(publication.date).getFullYear() == year)).sort((a, b) => (new Date(b.date)).getMonth() - (new Date(a.date)).getMonth()).map(publication => {
+              return <div class="rounded flex flex-col m-2">
+                {/* @ts-ignore */}
+                <div class="_df_thumb df-book-cover m-0" id="flipbook_example" source={publication.file} thumb={publication.cover}></div>
+                <div class="pt-2">
+                  <p class="text-xs">{publication.name}</p>
+                  <p class="text-sm">{(new Date(publication.date).toLocaleDateString())}</p>
+                </div>
+              </div>
+            })}
+            </div>
+          </div>
+      })}
     </div>
   );
 }
--- src/publications.ts
+++ src/publications.ts
@@ -1,7 +1,33 @@
 export default [
   {
     "name": "Cache Feb 2024",
-    "date": "",
-    "file": "/publications/Cache_Feb_2024.pdf"
-  }
+    "date": "2024-02-14T00:00:00.000+05:30",
+    "file": "/publications/Cache_Feb_2024.pdf",
+    "cover": "/publications/Cache_Feb_2024.png",
+    "highlight": "New Edition",
+  },
+  {
+    "name": "Cache Dec 2021",
+    "date": "2021-12-01T00:00:00.000+05:30",
+    "file": "/publications/Cache_Dec_2021.pdf",
+    "cover": "/publications/Cache_Dec_2021.png"
+  },
+  {
+    "name": "Cache Feb 2022",
+    "date": "2022-02-01T00:00:00.000+05:30",
+    "file": "/publications/Cache_Feb_2022.pdf",
+    "cover": "/publications/Cache_Feb_2022.png"
+  },
+  {
+    "name": "Cache Jan 2022",
+    "date": "2022-01-01T00:00:00.000+05:30",
+    "file": "/publications/Cache_Jan_2022.pdf",
+    "cover": "/publications/Cache_Jan_2022.png"
+  },
+  {
+    "name": "Cache Mar 2022",
+    "date": "2022-03-01T00:00:00.000+05:30",
+    "file": "/publications/Cache_Mar_2022.pdf",
+    "cover": "/publications/Cache_Mar_2022.png"
+  },
 ]
