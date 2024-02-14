372c36e3e1a88422108c7af540ff14fa2fb55bb6
Muthu Palaniyappan OL <muthu892542@gmail.com>
2024-02-14 08:51:34 +0530

    Init

---
+++ src/app.tsx
@@ -0,0 +1,23 @@
+import { useState } from "preact/hooks";
+import publications from "./publications";
+
+// prettier-ignore
+export function App() {
+  const [index, _] = useState(0);
+
+  return (
+    <div class="font-Azonix">
+      <Nav />
+      {/* @ts-ignore */}
+      <div class="_df_book w-full" id="flipbook_example" source={publications[index].file}></div>
+    </div>
+  );
+}
+
+function Nav() {
+  return (
+    <div class="p-2 bg-slate-400">
+      <p>Cache 2024</p>
+    </div>
+  );
+}
---
+++ src/publications.ts
@@ -0,0 +1,7 @@
+export default [
+  {
+    "name": "Cache Feb 2024",
+    "date": "",
+    "file": "/publications/Cache_Feb_2024.pdf"
+  }
+]
