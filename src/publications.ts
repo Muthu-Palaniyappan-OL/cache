8dd837a5114486c8c91af06bbc89064a22fdd646
Muthu Palaniyappan OL <muthu892542@gmail.com>
2024-02-14 21:32:03 +0530

    PNG 2 WEBP

--- src/publications.ts
+++ src/publications.ts
@@ -3,37 +3,37 @@ export default [
     "name": "Cache Feb 2024",
     "date": "2024-02-14T00:00:00.000+05:30",
     "file": "/publications/Cache_Feb_2024.pdf",
-    "cover": "/publications/Cache_Feb_2024.png",
+    "cover": "/publications/Cache_Feb_2024.webp",
     "highlight": "New",
   },
   {
     "name": "Cache Dec 2021",
     "date": "2021-12-01T00:00:00.000+05:30",
     "file": "/publications/Cache_Dec_2021.pdf",
-    "cover": "/publications/Cache_Dec_2021.png"
+    "cover": "/publications/Cache_Dec_2021.webp"
   },
   {
     "name": "Cache Feb 2022",
     "date": "2022-02-01T00:00:00.000+05:30",
     "file": "/publications/Cache_Feb_2022.pdf",
-    "cover": "/publications/Cache_Feb_2022.png"
+    "cover": "/publications/Cache_Feb_2022.webp"
   },
   {
     "name": "Cache Jan 2022",
     "date": "2022-01-01T00:00:00.000+05:30",
     "file": "/publications/Cache_Jan_2022.pdf",
-    "cover": "/publications/Cache_Jan_2022.png"
+    "cover": "/publications/Cache_Jan_2022.webp"
   },
   {
     "name": "Cache Mar 2022",
     "date": "2022-03-01T00:00:00.000+05:30",
     "file": "/publications/Cache_Mar_2022.pdf",
-    "cover": "/publications/Cache_Mar_2022.png"
+    "cover": "/publications/Cache_Mar_2022.webp"
   },
   {
     "name": "Cache Nov 2022",
     "date": "2022-11-01T00:00:00.000+05:30",
     "file": "/publications/Cache_Nov_2022.pdf",
-    "cover": "/publications/Cache_Nov_2022.png"
+    "cover": "/publications/Cache_Nov_2022.webp"
   },
 ]
